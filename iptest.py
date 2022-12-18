389c7f9d1e4f5951d02e2046e14f7e6f7977b41e
qin.batista <qin.batista@outlook.com>
2022-12-17 23:37:33 -0800

    Update iptest.py

--- iptest.py
+++ iptest.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 import os
 import sys
+import requests
+
+def display_ip(url):
+    response = requests.get(url).json()
+    for key, value in response.items():
+        print(key, value)
+
+
 if __name__ == "__main__":
-	if len(sys.argv)>=2:
-		os.system('curl http://us.qinyupeng.com:7031/ip/'+sys.argv[1])
-	else:
-		os.system('curl http://us.qinyupeng.com:7031/ip/myself')
+    if len(sys.argv) >= 2:
+        display_ip('http://us.qinyupeng.com:7031/ip/'+sys.argv[1])
+    else:
+        display_ip('http://us.qinyupeng.com:7031/ip/myself')
