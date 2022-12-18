34803131b82540b7637b873c140f72c827050c71
MacStudio <qin.batista@outlook.com>
2022-12-17 22:54:11 -0800

    update

--- iptest.py
+++ iptest.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
 if __name__ == "__main__":
 	if len(sys.argv)>=2:
-		os.system('curl http://www.cip.cc/'+sys.argv[1])
+		os.system('curl http://us.qinyupeng.com:7031/ip/'+sys.argv[1])
 	else:
-		os.system('curl http://www.cip.cc')
+		os.system('curl http://us.qinyupeng.com:7031/ip/myself')
